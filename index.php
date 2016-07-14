059f61abefeb0a670f3327da0479530c5a12f7d8
amidfdssdfds <8vmfjadxq@30mail.ir>
2016-07-14 19:52:52 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
-define('API_KEY','XXXX');
-$the_admin_id = 0;
+define('API_KEY','239101742:AAFmVJtUm-yNToe3hNoJKE8vNTjgT5yIygg');
+$the_admin_id = 95876809;
 
 file_put_contents("count",file_get_contents("count")+1);
 
